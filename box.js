a553bfac4604f39094b6d8d1bc00daafee51b161
pkivolowitz <perry@kivolowitz.com>
2019-06-12 06:15:23 -0500

    something that renders something

---
+++ box.js
@@ -0,0 +1,54 @@
+class Box
+{
+	constructor()
+	{
+		this.vrts = [];
+		this.vrts_buffer = null;
+	}
+
+	Initialize()
+	{
+		this.vrts_buffer = gl.createBuffer();
+		this.PushVertex(this.vrts, vec3.fromValues(-1,  1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues( 1,  1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues( 1,  1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues( 1, -1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues( 1, -1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues(-1, -1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues(-1, -1, 0));
+		this.PushVertex(this.vrts, vec3.fromValues(-1,  1, 0));
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vrts_buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vrts), gl.STATIC_DRAW);
+		gl.bindBuffer(gl.ARRAY_BUFFER, null);
+		this.InitializeShader();				
+	}
+
+	PushVertex(a, v)
+	{
+		a.push(v[0], v[1], v[2]);
+	}
+
+	InitializeShader()
+	{
+		this.p_shader = CreateShader("vertex_shader", "fragment_shader");
+		gl.useProgram(this.p_shader);
+		this.p_coordinates = gl.getAttribLocation(this.p_shader, "vertex_coordinates");
+		this.p_modelview_matrix_handle = gl.getUniformLocation(this.p_shader, "modelview_matrix");
+		this.p_projection_matrix_handle = gl.getUniformLocation(this.p_shader, "projection_matrix");
+		gl.useProgram(null);
+	}
+
+	Draw(modelview_matrix, projection_matrix)
+	{
+		gl.useProgram(this.p_shader);
+		gl.uniformMatrix4fv(this.p_projection_matrix_handle, false, projection_matrix);
+		gl.uniformMatrix4fv(this.p_modelview_matrix_handle, false, modelview_matrix);		
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vrts_buffer);
+		gl.vertexAttribPointer(this.p_coordinates, 3, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.p_coordinates);
+		gl.drawArrays(gl.LINES, 0, this.vrts.length / 3);
+		gl.disableVertexAttribArray(this.p_coordinates);
+		gl.useProgram(null);
+	}
+}
+
---
+++ shaders.js
@@ -0,0 +1,52 @@
+/**
+* Compile and link shader programs
+* @param {string} id string for the vertex shader
+* @param {string} id string for the fragment shader 
+* @return {Number} the gl shader program number
+*
+* Some elements drawn from: https://webglfundamentals.org/webgl/lessons/webgl-boilerplate.html
+*/
+function CreateShader(vrtx_id, frag_id)
+{
+	if (!vrtx_id)
+		throw"Parameter 1 to CreateShader may be missing.";
+
+	if (!frag_id)
+		throw "Parameter 2 to CreateShader may be missing.";
+
+	var success;
+
+	var vrtx = document.getElementById(vrtx_id);
+	if (!vrtx)
+		throw "Could not find script element " + vrtx_id;
+	vrtx = vrtx.text;
+
+	var frag = document.getElementById(frag_id);
+	if (!frag)
+		throw "Could not find script element " + frag_id;
+	frag = frag.text;
+
+	var vertShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertShader, vrtx);
+	gl.compileShader(vertShader);
+	success = gl.getShaderParameter(vertShader, gl.COMPILE_STATUS);
+	if (!success)
+		throw "Could not compile vertex shader:" + gl.getShaderInfoLog(vertShader);
+
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragShader, frag);
+	gl.compileShader(fragShader);
+	success = gl.getShaderParameter(fragShader, gl.COMPILE_STATUS);
+	if (!success)
+		throw "Could not compile fragment shader:" + gl.getShaderInfoLog(fragShader);
+
+	var shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertShader); 
+	gl.attachShader(shaderProgram, fragShader);
+	gl.linkProgram(shaderProgram);
+	success = gl.getProgramParameter(shaderProgram, gl.LINK_STATUS);
+	if (!success)
+		throw ("Shader program filed to link:" + gl.getProgramInfoLog (shaderProgram));
+		
+	return shaderProgram;
+}
